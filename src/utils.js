404fa9b3d4bdbcbe2b1ef2d7b3818ed7e57c0c13
Mickey Burks <brks.mck@gmail.com>
2017-07-14 11:16:24 -0500

    Add functional helpers

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,32 @@
 /**
  * @param {Object} data
  * @param {String} queryKey - key to extract from query string
- * @returns {Function} - route callback sending json
+ * @returns {Function} - express handler
  */
 export function mapQueryParamToKey (data, queryKey) {
   return function (req, res) {
     const queryValue = req.query[queryKey]
+    send(res, data[queryValue])
     res.json(data[queryValue])
   }
 }
 
+export function send (res, data) {
+  res.send(data)
+}
+
+export function wrap (key, data) {
+  return { [key]: data }
+}
+
+export function dWrap (data) {
+  return wrap('d', data)
+}
+
 /**
+ * express handler
  * Return the body of the POST, wrapped in a { d: [body] }
+ * @returns {void}
  */
 export function mirrorBody (req, res) {
   // Node doesn't support the spread operator on objects, so...
@@ -20,5 +35,5 @@ export function mirrorBody (req, res) {
       acc[current] = req.body[current]
       return acc
     }, {})
-  res.send({ d: response })
+  send(res, dWrap(response))
 }
