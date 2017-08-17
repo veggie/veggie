d9f9177d42a2f2074b32d3629612074eb142eb5a
Mickey Burks <mickey@burks.online>
2017-08-17 17:26:59 -0500

    matchKeyToQueryParam util

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,12 @@
 /**
- * @param {Object} data
- * @param {String} queryKey - key to extract from query string
- * @returns {Function} - express handler
+ * @param {object} data
+ * @param {string} queryKey - key to extract from query string
+ * @returns {function} - express handler
  */
 export function mapQueryParamToKey (data, queryKey) {
   return function (req, res) {
     const queryValue = req.query[queryKey]
     send(res, data[queryValue])
-    res.json(data[queryValue])
   }
 }
 
@@ -36,4 +35,69 @@ export function mirrorBody (req, res) {
       return acc
     }, {})
   send(res, dWrap(response))
+}
+
+/**
+ * @param {string} queryKey
+ * @param {object} responses - { String: JSON|Express route|path }
+ * @returns {function} - express handler
+ */
+export function matchKeyToQueryParam (queryKey, responses) {
+  return (req, res) => {
+    const queryValue = req.query[queryKey]
+    const responseKey = Object.keys(responses)
+      .find(pattern => {
+        const regex = new RegExp(pattern)
+        return regex.test(queryValue)
+      })
+    const callback = getRouteHandler(responses[responseKey])
+    callback(res)
+  }
+}
+
+/**
+ * Returns an express route handler for the given service
+ *
+ * @param {function|object|string} response - The service response
+ * @returns {(req: Express Request, res: Express Response) => void}
+ */
+export function getRouteHandler (response) {
+  if (typeof response === 'function') {
+    // Express route
+    return response
+  }
+
+  if (typeof response === 'string') {
+    // Path to json file
+    return (req, res) => {
+      const data = cachelessRequire(response)
+      if (data) {
+        res.json(data)
+      }
+      else {
+        res.status(404).json({})
+      }
+    }
+  }
+
+  // JSON object
+  const data = response
+  return (req, res) => {
+    if (data) {
+      res.json(data)
+    }
+    else {
+      res.status(404).json({})
+    }
+  }
+}
+
+/**
+ * @param {string} filePath - absolute file path to load
+ * @returns {module} - required file
+ */
+function cachelessRequire (filePath) {
+  const data = require(filePath)
+  delete require.cache[filePath]
+  return data
 }
