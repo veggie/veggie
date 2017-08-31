c6d9d37022b0c197d5debd00db0faf9df86cb489
Mickey Burks <brks.mck@gmail.com>
2017-08-31 15:05:24 -0500

    Require user to set delay on middleware

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const middlewarePassThroughCode = 501
 function middleware ({ dir, time = MAX_DELAY, profile = null }) {
   const config = arguments[0]
   config.catchAllStatusCode = middlewarePassThroughCode
-  config.time = 0
 
   // Load intial profile
   if (profile) {
