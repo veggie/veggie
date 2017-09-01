33cb808d2ee187380556c3bf10873d995d361f5e
Mickey Burks <brks.mck@gmail.com>
2017-09-01 15:14:41 -0500

    Move api to module

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import pathToRegexp from 'path-to-regexp'
+import * as apiMethods from './methods'
+import { apiPathPrefix } from '../common'
+
+const middlewareApiRegex = pathToRegexp(`${apiPathPrefix}/:method/:arg?`)
+
+/**
+ * Middleware that registers the profile api
+ * @returns {function}
+ */
+function apiMiddleware () {
+  const apiRouter = express.Router()
+  Object.keys(apiMethods)
+    .forEach(method => {
+      apiRouter.all(`${apiPathPrefix}/${method}/:arg?`, apiRoutes[`${method}Route`])
+    })
+  return (req, res, next) {
+    if (middlewareApiRegex.test(req.path)) {
+      apiRouter(req, res, next)
+    } else {
+      next()
+    }
+  }
+}
+
+const apiRoutes = {
+  // TODO
+}
