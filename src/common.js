2e6a325331e03faa351cbfeae75744b6c65fd32f
Mickey Burks <mickey@burks.online>
2017-09-01 14:46:49 -0500

    Add socket path in common

--- src/common.js
+++ src/common.js
@@ -1,2 +1,12 @@
 // Path for API
 export const apiPathPrefix = '/service-profile/api'
+
+// Path for repl socket
+export const socketPath = '/tmp/service-profile.sock'
+
+/**
+ * Return random delay from 0 to time, exclusive
+ * @param {number} time - max delay
+ * @returns {number}
+ */
+export const randomExclusive = time => Math.floor(Math.random() * time)
