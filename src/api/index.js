98002e317bd9592f810caa28c472d487129943ff
Mickey Burks <brks.mck@gmail.com>
2017-09-01 16:48:39 -0500

    Add route handler for api calls

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ export function apiMiddleware () {
   const apiRouter = express.Router()
   Object.keys(apiMethods)
     .forEach(method => {
-      apiRouter.all(`${apiPathPrefix}/${method}/:arg?`, apiRoutes[`${method}Route`])
+      apiRouter.all(`${apiPathPrefix}/${method}/:arg?`, apiHandler)
     })
   return (req, res, next) => {
     if (middlewareApiRegex.test(req.path)) {
@@ -26,6 +26,25 @@ export function apiMiddleware () {
   }
 }
 
-const apiRoutes = {
-  // TODO
+/**
+ * Route handler for api calls
+ * @returns {void}
+ */
+function apiHandler (req, res) {
+  const { body, params } = req
+  const { method, arg } = params
+
+  try {
+    if (body) {
+      apiMethods[method](body)
+    } else {
+      apiMethods[method](arg)
+    }
+    res.status(200)
+    res.send({ message: `mock-server: ${method} call successful` })
+  } catch (e) {
+    res.status(500)
+    res.send({ message: `mock-server: ${method} call failed` })
+  }
+  res.end()
 }
