7275eb418b4cd4707e010e192f4d41da68eb4d0c
Mickey Burks <brks.mck@gmail.com>
2017-09-05 16:11:25 -0500

    Get router and store hash of override, get override route handler

--- src/profile.js
+++ src/profile.js
@@ -1,4 +1,9 @@
-import { getServices, profileRouter, matchServices } from './api'
+import { serviceOverrides } from './api/services'
+import { getRouteHandler } from './utils'
+import crypto from 'crypto'
+
+let profileRouter
+let cachedProfileHash
 
 /**
  * Middleware that registers profile override responses
@@ -6,7 +11,48 @@ import { getServices, profileRouter, matchServices } from './api'
  */
 export function profileOverrideMiddleware (profile) {
   return (req, res, next) => {
-    // let profileRouter = getProfileRouter()
+    profileRouter = getRouter()
     profileRouter(req, res, next)
   }
+}
+
+/**
+ * Get router representing the currently loaded profile
+ * @returns {Express router}
+ */
+function getRouter () {
+  const profileHash = getProfileHash()
+  if (profileHash === cachedProfileHash) {
+    return profileRouter
+  }
+
+  // cache profile hash
+  cachedProfileHash = profileHash
+
+  // create new router
+  const router = express.Router()
+  Object.keys(serviceOverrides)
+    .forEach(url => {
+      router.all(url, getOverrideHandler(serviceOverrides[url]))
+    })
+  return router
+}
+
+/**
+ * Get a hash of the currently loaded profile
+ * @returns {string} - hash of current service overrides object
+ */
+function getProfileHash () {
+  const hash = crypto.createHash('md5')
+  hash.update(JSON.stringify(serviceOverrides))
+  return hash.digest('hex')
+}
+
+/**
+ * Parse profile override and get route handler function
+ * @param {object} override - profile override to parse
+ * @returns {Express route} - route handler
+ */
+function getOverrideHandler ({ response, status }) {
+  return getRouteHandler(response, status)
 }
