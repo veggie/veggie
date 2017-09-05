3426160b23ffb67eaf1b04747defc555172d761f
Mickey Burks <brks.mck@gmail.com>
2017-09-05 16:10:47 -0500

    Use correct paths, cleanup

--- src/log.js
+++ src/log.js
@@ -1,14 +1,14 @@
 import chalk from 'chalk'
 import { randomExclusive } from './common'
 
-const { blue, green, red, yellow, magenta, cyan, white, underline, gray } = chalk
+const { blue, green, red, underline, gray } = chalk
 
 const log  = message => console.log(green(message))
 const error = message => console.log(red(message))
 
 const mock = type => blue(`${type}:`)
-const colors = [ red, blue, green, yellow, magenta, cyan, white, gray ]
-const randomColor = message => colors[randomExlusive(colors.length)](message)
+const colors = ['red','blue','green','yellow','magenta','cyan','white','gray']
+const randomColor = message => chalk[colors[randomExlusive(colors.length)]](message)
 const random = message => message.split(' ').map(word => randomColor(word)).join(' ')
 
 const profile = mock('service-profile')
