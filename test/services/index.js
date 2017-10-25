7c3d95a0b4dcfa9b9b6e4c883d9d0ca3b46be526
Mickey Burks <brks.mck@gmail.com>
2017-10-25 16:25:58 -0500

    Update tests

--- test/services/index.js
+++ test/services/index.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
 module.exports = {
-  '/route': {}
+  '/path': path.join(__dirname, '../data/sample.json'),
+  '/obj': { msg: 'obj' },
+  '/fn': (req, res) => res.send({ msg: 'fn' }),
+  '/fn/params/:id': (req, res) => res.send({ msg: 'fn', params: req.params }),
+  '/fn/query': (req, res) => res.send({ msg: 'fn', query: req.query }),
+  '/fn/body': (req, res) => res.send({ msg: 'fn', body: req.body })
 }
