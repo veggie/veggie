5b32a987d00dace7cc3386e6bec4b32be37f8cdf
Mickey Burks <brks.mck@gmail.com>
2017-10-25 16:29:17 -0500

    Encode/decode uri arguments in api

--- src/fetchClientApi.js
+++ src/fetchClientApi.js
@@ -8,6 +8,19 @@ import { apiPathPrefix } from './common'
 // @returns {function}
 const api = method => (...args) => fetch(`${apiPathPrefix}/${[method, ...args].join('/')}`)
 
+/**
+ * Return method to call api function via fetch
+ * @returns {function}
+ */
+function api (method) {
+  return function (...args) {
+    const encodedArgs = args.map(arg => encodeURIComponent(arg))
+    const path = [method].concat(encodedArgs).join('/')
+    return fetch(`${hostname}${apiPathPrefix}/${path}`)
+  }
+}
+
+export const set = api('set')
 export const block = api('block')
 export const reset = api('reset')
 export const blockAll = api('blockAll')
