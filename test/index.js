9566b458650f2df1a882a8f2d8b72cc5c2b305b6
Mickey Burks <brks.mck@gmail.com>
2017-12-08 15:28:15 -0600

    Make tests summaries more readable, add future-facing params tests

--- test/index.js
+++ test/index.js
@@ -53,136 +53,191 @@ describe('a server', () => {
         app.emit('close')
       })
 
-      it('handles path and doesn\'t cache file', () => {
-        return fetchJSON('/path')
-          .then(({ msg }) => {
-            assert(msg === 'path')
+      describe('handles services config', () => {
+        describe('from route path', () => {
+          let msg
 
+          before(async () => {
+            try {
+              const res = await fetchJSON('/path')
+              msg = res.msg
+            } catch (e) {
+              assert(false)
+            }
+          })
+
+          it('by returning correct data', () => {
+            assert(msg === 'path')
+          })
+
+          it('doesn\'t cache file', () => {
             const test = path.join(__dirname, 'data/test.json')
+
             // Test that file has not been cached
             assert(typeof require.cache[test] === 'undefined')
           })
-          .catch(() => assert(false)) // Fail
-      })
+        })
 
-
-      it('handles static object', () => {
-        return fetchJSON('/obj')
-          .then(({ msg }) => assert(msg === 'obj'))
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('handles route function', () => {
-        return fetchJSON('/fn')
-          .then(({ msg }) => assert(msg === 'fn'))
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('handles route function with params', () => {
-        return fetchJSON('/fn/params/123')
-          .then(({ msg, params }) => {
-            assert(msg === 'fn')
-            assert(typeof params !== 'undefined')
-            assert(params.id === '123')
+        describe('from static JSON object', () => {
+          it('by returning correct data', () => {
+            return fetchJSON('/obj')
+              .then(({ msg }) => assert(msg === 'obj'))
+              .catch(() => assert(false)) // Fail
           })
-          .catch(() => assert(false)) // Fail
-      })
+        })
 
-      it('handles route function with query', () => {
-        return fetchJSON('/fn/query?id=123')
-          .then(({ msg, query }) => {
-            assert(msg === 'fn')
-            assert(typeof query !== 'undefined')
-            assert(query.id === '123')
-          })
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('handles route function with body', () => {
-        return fetchJSON('/fn/body', { id: 123 })
-          .then(({ msg, body }) => {
-            assert(msg === 'fn')
-            assert(typeof body !== 'undefined')
-            assert(body.id === 123)
-          })
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('can block services', () => {
-        return veggieApi.block('/obj')
-          .then(() => fetchJSON('/obj'))
-          .catch(err => {
-            // Catch 404 error
-            assert(true)
-          })
-      })
-
-      it('can reset blocked services', () => {
-        return veggieApi.block('/obj')
-          .then(() => fetchJSON('/obj'))
-          .catch(err => {
-            // Catch 404 error
-            assert(true)
-            return veggieApi.reset('/obj')
-          })
-          .then(() => fetchJSON('/obj'))
-          .then(({ msg }) => assert(msg === 'obj'))
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('can set a service response', () => {
-        return veggieApi.set('/obj', 200, { msg: 'set' })
-          .then(() => fetchJSON('/obj'))
-          .then(({ msg }) => {
-            assert(msg === 'set')
-          })
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('can set a service status code', () => {
-        return veggieApi.set('/obj', 400, {})
-          .then(() => fetchJSON('/obj'))
-          .then(() => assert(false)) // Fail
-          .catch(e => {
-            assert(/400/.test(e))
-          })
-      })
-
-      it('can set a service that isn\'t specified by mock data', () => {
-        return veggieApi.set('/set', 200, { msg: 'set' })
-          .then(() => fetchJSON('/set'))
-          .then(({ msg }) => {
-            assert(msg === 'set')
-          })
-          .catch(() => assert(false)) // Fail
-      })
-
-      it('can load a profile', () => {
-        return veggieApi.load('test')
-          .then(() => fetchJSON('/obj'))
-          .then(() => assert(false)) // Fail
-          .catch(e => assert(/409/.test(e)))
-      })
-
-      it('can save a profile', () => {
-        return veggieApi.save('newTest')
-          .then(() => {
-            assert(fs.existsSync(path.join(__dirname, 'profiles/newTest.json')))
-            fs.unlinkSync(path.join(__dirname, 'profiles/newTest.json'))
-          })
-          .catch(() => assert(false))
-      })
-
-      it('can hang a service', () => {
-        return veggieApi.hang('/obj')
-          .then(() => {
-            return new Promise((resolve, reject) => {
-              setTimeout(resolve, 1000)
-
-              fetchJSON('/obj')
-                .then(() => reject(new Error('request did not hang')))
+        describe('from route function', () => {
+          describe('with no special function', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn')
+                .then(({ msg }) => assert(msg === 'fn'))
+                .catch(() => assert(false)) // Fail
             })
           })
+
+          describe('with params', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn/params/123')
+                .then(({ msg, type, params }) => {
+                  assert(msg === 'fn')
+                  assert(type === 'no-question')
+                  assert(typeof params !== 'undefined')
+                  assert(params.id === '123')
+                })
+                .catch(() => assert(false)) // Fail
+            })
+          })
+
+          describe('with params including a ?', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn/params/123?')
+                .then(({ msg, type, params }) => {
+                  assert(msg === 'fn')
+                  assert(type === 'question')
+                  assert(typeof params !== 'undefined')
+                  assert(params.id === '123')
+                })
+                .catch(() => assert(false)) // Fail
+            })
+          })
+
+          describe('with params including a ? and query param', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn/params/123?search=true')
+                .then(({ msg, type, params }) => {
+                  assert(msg === 'fn')
+                  assert(type === 'question-with-query')
+                  assert(typeof params !== 'undefined')
+                  assert(params.id === '123')
+                })
+                .catch(() => assert(false)) // Fail
+            })
+          })
+
+          describe('with query', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn/query?id=123')
+                .then(({ msg, query }) => {
+                  assert(msg === 'fn')
+                  assert(typeof query !== 'undefined')
+                  assert(query.id === '123')
+                })
+                .catch(() => assert(false)) // Fail
+            })
+          })
+
+          describe('with body', () => {
+            it('by returning correct data', () => {
+              return fetchJSON('/fn/body', { id: 123 })
+                .then(({ msg, body }) => {
+                  assert(msg === 'fn')
+                  assert(typeof body !== 'undefined')
+                  assert(body.id === 123)
+                })
+                .catch(() => assert(false)) // Fail
+            })
+          })
+        })
+      })
+
+
+      describe('api', () => {
+        it('can block services', () => {
+          return veggieApi.block('/obj')
+            .then(() => fetchJSON('/obj'))
+            .catch(err => {
+              // Catch 404 error
+              assert(true)
+            })
+        })
+
+        it('can reset blocked services', () => {
+          return veggieApi.block('/obj')
+            .then(() => fetchJSON('/obj'))
+            .catch(err => {
+              // Catch 404 error
+              assert(true)
+              return veggieApi.reset('/obj')
+            })
+            .then(() => fetchJSON('/obj'))
+            .then(({ msg }) => assert(msg === 'obj'))
+            .catch(() => assert(false)) // Fail
+        })
+
+        it('can set a service response', () => {
+          return veggieApi.set('/obj', 200, { msg: 'set' })
+            .then(() => fetchJSON('/obj'))
+            .then(({ msg }) => {
+              assert(msg === 'set')
+            })
+            .catch(() => assert(false)) // Fail
+        })
+
+        it('can set a service status code', () => {
+          return veggieApi.set('/obj', 400, {})
+            .then(() => fetchJSON('/obj'))
+            .then(() => assert(false)) // Fail
+            .catch(e => {
+              assert(/400/.test(e))
+            })
+        })
+
+        it('can set a service that isn\'t specified by mock data', () => {
+          return veggieApi.set('/set', 200, { msg: 'set' })
+            .then(() => fetchJSON('/set'))
+            .then(({ msg }) => {
+              assert(msg === 'set')
+            })
+            .catch(() => assert(false)) // Fail
+        })
+
+        it('can load a profile', () => {
+          return veggieApi.load('test')
+            .then(() => fetchJSON('/obj'))
+            .then(() => assert(false)) // Fail
+            .catch(e => assert(/409/.test(e)))
+        })
+
+        it('can save a profile', () => {
+          return veggieApi.save('newTest')
+            .then(() => {
+              assert(fs.existsSync(path.join(__dirname, 'profiles/newTest.json')))
+              fs.unlinkSync(path.join(__dirname, 'profiles/newTest.json'))
+            })
+            .catch(() => assert(false))
+        })
+
+        it('can hang a service', () => {
+          return veggieApi.hang('/obj')
+            .then(() => {
+              return new Promise((resolve, reject) => {
+                setTimeout(resolve, 1000)
+
+                fetchJSON('/obj')
+                  .then(() => reject(new Error('request did not hang')))
+              })
+            })
+        })
       })
     })
   })
--- test/services/index.js
+++ test/services/index.js
@@ -4,7 +4,9 @@ module.exports = {
   '/path': path.join(__dirname, '../data/test.json'),
   '/obj': { msg: 'obj' },
   '/fn': (req, res) => res.send({ msg: 'fn' }),
-  '/fn/params/:id': (req, res) => res.send({ msg: 'fn', params: req.params }),
+  '/fn/params/:id': (req, res) => res.send({ msg: 'fn', type: 'no-question', params: req.params }),
+  '/fn/params/:id?': (req, res) => res.send({ msg: 'fn', type: 'question', params: req.params }),
+  '/fn/params/:id?search=true': (req, res) => res.send({ msg: 'fn', type: 'question-with-query', params: req.params }),
   '/fn/query': (req, res) => res.send({ msg: 'fn', query: req.query }),
   '/fn/body': (req, res) => res.send({ msg: 'fn', body: req.body })
 }
