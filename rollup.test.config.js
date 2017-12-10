97df73e8e5cd326e5242a6ba456cf5658ee74e0c
Mickey Burks <mickey@burks.online>
2017-12-10 02:12:23 -0600

    Bundle tests to enable better coverage reporting

---
+++ rollup.test.config.js
@@ -0,0 +1,39 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+import istanbul from 'rollup-plugin-istanbul'
+
+const external = [
+  'body-parser',
+  'chalk',
+  'crypto',
+  'express',
+  'fs',
+  'get-port',
+  'glob',
+  'http',
+  'meow',
+  'net',
+  'path',
+  'path-to-regexp',
+  'repl',
+  'url'
+]
+
+export default [
+  // Test server
+  {
+    external,
+    input: './test/index.js',
+    output: { file: './dist/veggie.test.js', format: 'cjs' },
+    plugins: [
+      json(),
+      istanbul({
+        include: 'src/**/*.js',
+      }),
+      babel(),
+      nodeResolve(),
+    ]
+  }
+]
+
