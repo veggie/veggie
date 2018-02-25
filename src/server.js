6d46d4ca4f679a63bc3a2d4566cb0ce5fced1e90
Mickey Burks <mickey@burks.online>
2018-02-25 11:29:41 -0600

    fix(server): Accept Content-Type in CORS headers

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ export const apiPath = `${apiPathPrefix}/${apiVersion}`
 
 apiRouter.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
 
