63ef81e229c2d8ea682971e1b5fcec60cb3bd1a5
Mickey Burks <mickey@burks.online>
2018-05-13 12:52:07 -0500

    fix(CORS): Allow all methods in headers

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ export const apiPath = `${apiPathPrefix}/${apiVersion}`
 
 apiRouter.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
